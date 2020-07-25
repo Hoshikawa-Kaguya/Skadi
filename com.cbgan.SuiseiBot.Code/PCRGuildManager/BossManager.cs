15b88e650260deb8b8fc47c9f6dec3afdfe7028d
Watremons <z2331355763@sina.com>
2020-07-25 20:51:34 +0800

    July 23: 完善网络文件下载和br文件解压功能,删除数据库

--- com.cbgan.SuiseiBot.Code/PCRGuildManager/BossManager.cs
+++ com.cbgan.SuiseiBot.Code/PCRGuildManager/BossManager.cs
@@ -26,56 +26,7 @@ namespace com.cbgan.SuiseiBot.Code.PCRGuildManager
             //数据库实例
             BossDBHelper bossDB = new BossDBHelper(Sender, GBossEventArgs);
 
-            //查找是否存在这个公会
-            if (!bossDB.GuildExists())
-            {
-                ConsoleLog.Debug("GuildExists", "guild not found");
-                qqGroup.SendGroupMessage(CQApi.CQCode_At(senderQQ.Id),
-                                         "\r\n此群未被登记为公会",
-                                         "\r\n请使用以下指令创建公会",
-                                         $"\r\n{PCRGuildHandle.GetCommandHelp(commandType)}");
-                return;
-            }
-            //TODO 检查是否存在公会
-
-            switch (commandType)
-            {
-                case PCRGuildCommandType.UpdateBoss:
-                    //检查执行者权限
-                    if (memberInfo.MemberType == QQGroupMemberType.Member)
-                    {
-                        //执行者为普通群员时拒绝执行指令
-                        qqGroup.SendGroupMessage(CQApi.CQCode_At(senderQQ.Id),
-                                                 "此指令只允许管理者执行");
-                        ConsoleLog.Warning($"会战[群:{qqGroup.Id}]", $"群成员{memberInfo.Nick}正在尝试执行指令{commandType}");
-                        return;
-                    }
-                    ConsoleLog.Info($"会战[群:{qqGroup.Id}]", $"开始处理指令{commandType}");
-
-                    int result = bossDB.StartLoadBoss();
-
-                    //判断返回值
-                    switch (result)
-                    {
-                        case -1://已经执行过开始命令
-                            qqGroup.SendGroupMessage(CQApi.CQCode_At(senderQQ.Id),
-                                                     "\r\n发现上期会战Boss数据了惹",
-                                                     "\r\n请检查是否未结束上期会战哦");
-                            break;
-                        case 0:
-                            qqGroup.SendGroupMessage(CQApi.CQCode_AtAll(),
-                                                     "\r\n成功加载Boss数据，快来康康吧！");
-                            break;
-                        default:
-                            qqGroup.SendGroupMessage(CQApi.CQCode_At(senderQQ.Id),
-                                                     "\r\nERROR",
-                                                     "\r\n遇到未知错误,请联系当前机器人维护者");
-                            break;
-                    }
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
+            //接下来进行一系列操作
         }
         #endregion
     }
