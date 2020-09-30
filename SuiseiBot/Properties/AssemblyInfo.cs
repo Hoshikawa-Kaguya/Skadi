fb0577fc1c89a6e18dc59dfb67a03a5f34cb4cef
脆饼干 <cbgancna@gmail.com>
2020-09-30 20:45:58 +0800

    更新版本号

--- SuiseiBot/Properties/AssemblyInfo.cs
+++ SuiseiBot/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.3.0.0930")]
-[assembly: AssemblyFileVersion("0.2.2.0923")]
+[assembly: AssemblyFileVersion("0.3.0.0930")]
