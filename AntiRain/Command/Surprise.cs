18bd710964ab4003f44b5468e20f87d5eef7c0df
Yukari316 <cbgancna@gmail.com>
2021-04-06 20:11:48 +0800

    抽老婆！

--- AntiRain/Command/Surprise.cs
+++ AntiRain/Command/Surprise.cs
@@ -1,31 +1,60 @@
 using System;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Sora.Attributes.Command;
 using Sora.Entities.CQCodes;
+using Sora.Enumeration.ApiType;
 using Sora.EventArgs.SoraEvent;
+using YukariToolBox.FormatLog;
 
 namespace AntiRain.Command
 {
     [CommandGroup]
-    internal class Surprise
+    public class Surprise
     {
         #region 私有方法
 
         [UsedImplicitly]
         [GroupCommand(CommandExpressions = new[] {"dice"})]
-        private async void RandomNumber(GroupMessageEventArgs eventArgs)
+        public async ValueTask RandomNumber(GroupMessageEventArgs eventArgs)
         {
-            Random randomGen = new();
+            Random rd = new();
             await eventArgs.SourceGroup.SendGroupMessage(CQCode.CQAt(eventArgs.Sender.Id), "丢出了\r\n",
-                                                         randomGen.Next(1, 6));
+                                                         rd.Next(1, 6));
         }
 
         [UsedImplicitly]
         [GroupCommand(CommandExpressions = new[] {"优质睡眠", "昏睡红茶", "昏睡套餐", "健康睡眠"})]
-        private async void RedTea(GroupMessageEventArgs eventArgs)
+        public async ValueTask RedTea(GroupMessageEventArgs eventArgs)
         {
             await eventArgs.SourceGroup.EnableGroupMemberMute(eventArgs.Sender.Id,
                                                               28800);
+        }
+        
+        [UsedImplicitly]
+        [GroupCommand(CommandExpressions = new[] {"抽老婆"})]
+        public async ValueTask RollWife(GroupMessageEventArgs eventArgs)
+        {
+            var (apiStatus, memberList) = await eventArgs.SourceGroup.GetGroupMemberList();
+            if (apiStatus != APIStatusType.OK)
+            {
+                Log.Error("api错误", $"api return {apiStatus}");
+                return;
+            }
+
+            //删除自身和发送者
+            memberList.RemoveAll(i => i.UserId == eventArgs.Sender);
+            memberList.RemoveAll(i => i.UserId == eventArgs.LoginUid);
+            
+            if (memberList.Count == 0) await eventArgs.Reply("群里没人是你的老婆");
+
+            await eventArgs.Reply("10秒后我将at一位幸运群友成为你的老婆\r\n究竟是谁会这么幸运呢");
+            await Task.Delay(10000);
+            var rd = new Random();
+            await eventArgs.Reply(CQCode.CQAt(memberList[rd.Next(0, memberList.Count - 1)].UserId),
+                                  "\r\n恭喜成为",
+                                  CQCode.CQAt(eventArgs.Sender), 
+                                  "的老婆 ~");
         }
 
         #endregion
